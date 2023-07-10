d2c6abb859b6792410110fabd42185dc2535fe38
Grunet <sadikshahidain@gmail.com>
2023-07-10 00:22:01 +0000

    fill in some more details

--- packages/honeypot/src/main.py
+++ packages/honeypot/src/main.py
@@ -1,7 +1,16 @@
 from servers import simple_http
+from servers.server_adapter_protocol import ServerAdapterProtocol
 
 if __name__ == "__main__":
-    print("Hello, world!")
+    serverAdapters: list[ServerAdapterProtocol] = []
 
-    c = simple_http.createServerAdapter()
-    c.start()
+    # TODO - use an environment variable here instead
+    if True:
+        sa = simple_http.createServerAdapter()
+        serverAdapters.append(sa)
+
+    for serverAdapter in serverAdapters:
+        serverAdapter.start()
+
+    # TODO - add keyboard interrupt handling
+    # TODO - add SIGTERM handling
