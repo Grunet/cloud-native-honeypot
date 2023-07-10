7831d395003f86e5d1c67415c6b1447fb6310389
Grunet <sadikshahidain@gmail.com>
2023-07-10 00:34:19 +0000

    fill in some more details

--- packages/honeypot/src/servers/simple_http.py
+++ packages/honeypot/src/servers/simple_http.py
@@ -1,14 +1,30 @@
 from .server_adapter_protocol import ServerAdapterProtocol
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
 
 class SimpleHttpServerAdapter(ServerAdapterProtocol):
+    def init(self):
+        self.__httpServer: HTTPServer | None = None
+
     def start(self):
-        print("start")
-        pass
+        print("Starting simple HTTP server on port 8000")
+
+        self.__httpServer = HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
+        self.__httpServer.serve_forever()
 
     def stop(self):
-        print("stop")
-        pass
+        print("Stopping simple HTTP server")
+
+        if self.__httpServer:
+            self.__httpServer.shutdown()
 
 
 def createServerAdapter() -> ServerAdapterProtocol:
