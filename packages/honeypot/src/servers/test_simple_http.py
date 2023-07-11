156b24f28a7e7856ec6a978d814ca23697e2da01
Grunet <sadikshahidain@gmail.com>
2023-07-11 03:31:16 +0000

    fill out an actual test

--- packages/honeypot/src/servers/test_simple_http.py
+++ packages/honeypot/src/servers/test_simple_http.py
@@ -1,9 +1,30 @@
+from .simple_http import createServerAdapter
+
 import unittest
+import http.client
 
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+class TestSimpleHttpServerAdapter(unittest.TestCase):
+    def setUp(self):
+        self.__serverAdapter = createServerAdapter()
+        self.__serverAdapter.start()
+
+    def tearDown(self):
+        self.__serverAdapter.stop()
+
+    def test_GET_returns_200(self):
+        # Arrange
+        conn = http.client.HTTPConnection("127.0.0.1:8000")
+
+        # Act
+        conn.request("GET", "/")
+
+        # Assert
+        response = conn.getresponse()
+        statusCode = response.getcode()
+        conn.close()
+
+        self.assertEqual(statusCode, 200)
 
 
 if __name__ == "__main__":
