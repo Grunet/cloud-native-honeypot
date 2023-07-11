be74e1ab26eba22bfa24648e548c99054804528c
Grunet <sadikshahidain@gmail.com>
2023-07-10 22:53:01 -0500

    Setup unit tests (#12)

---
+++ packages/honeypot/src/servers/test_simple_http.py
@@ -0,0 +1,31 @@
+from .simple_http import createServerAdapter
+
+import unittest
+import http.client
+
+
+class TestSimpleHttpServerAdapter(unittest.TestCase):
+    def setUp(self) -> None:
+        self.__serverAdapter = createServerAdapter()
+        self.__serverAdapter.start()
+
+    def tearDown(self) -> None:
+        self.__serverAdapter.stop()
+
+    def test_GET_returns_200(self) -> None:
+        # Arrange
+        conn = http.client.HTTPConnection("127.0.0.1:8000", timeout=5)
+
+        # Act
+        conn.request("GET", "/")
+
+        # Assert
+        response = conn.getresponse()
+        statusCode = response.getcode()
+        conn.close()  # unittest reports an unclosed socket even with this
+
+        self.assertEqual(statusCode, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
