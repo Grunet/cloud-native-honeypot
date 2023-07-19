8a14b04dd1bc243943a8fa452cea64d774a0b268
Grunet <sadikshahidain@gmail.com>
2023-07-19 02:42:45 +0000

    add in code for publishing the event

--- packages/honeypot/src/eventClients/eventbridge_client_adapter.py
+++ packages/honeypot/src/eventClients/eventbridge_client_adapter.py
@@ -2,13 +2,29 @@ from .event_client_adapter_protocol import EventClientAdapterProtocol
 
 import boto3
 
+import json
+
 
 class EventbridgeClientAdapter(EventClientAdapterProtocol):
     def __init__(self) -> None:
         self.__eventbridgeClient = boto3.client("events")
 
     def sendEvent(self, eventDetails: object) -> None:
-        raise NotImplementedError
+        response = self.__eventbridgeClient.put_events(
+            Entries=[
+                {
+                    "Source": "cloud-native-honeypot",
+                    "DetailType": "cloudNativeHoneypotTriggered",
+                    "Detail": json.dumps(eventDetails),
+                    "EventBusName": "default",
+                }
+            ]
+        )
+
+        if response["FailedEntryCount"] == 0:
+            print("Event published successfully.")
+        else:
+            print(f"Failed to publish {response['FailedEntryCount']} event(s).")
 
 
 def createEventClientAdapter() -> EventClientAdapterProtocol:
