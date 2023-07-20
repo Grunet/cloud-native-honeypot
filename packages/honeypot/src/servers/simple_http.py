0728c352c4787e7108fe1e1a5a65fef26e497737
Grunet <sadikshahidain@gmail.com>
2023-07-20 04:30:32 +0000

    clarify comments

--- packages/honeypot/src/servers/simple_http.py
+++ packages/honeypot/src/servers/simple_http.py
@@ -33,7 +33,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 class SimpleHttpServerAdapter(ServerAdapterProtocol):
     def __init__(self, **kwargs) -> None:
-        self.__eventClient = kwargs.get("eventClient")
+        self.__eventClient: EventClientAdapterProtocol | None = kwargs.get(
+            "eventClient"
+        )
 
         self.__httpServer: HTTPServer | None = None
 
@@ -41,7 +43,10 @@ class SimpleHttpServerAdapter(ServerAdapterProtocol):
         print("Starting simple HTTP server on port 8000")
 
         self.__httpServer = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-        self.__httpServer.eventClient = self.__eventClient
+        # Only clear way to inject the client into request handling
+        # is by appending it onto the server object
+        # and reading it later on
+        self.__httpServer.eventClient = self.__eventClient  # pyre-ignore[16]
 
         def start_in_separate_thread(httpServer: HTTPServer | None) -> None:
             if httpServer:
