e0c062c5ae24f98f3199e725e962dc7afc14d2d4
Grunet <sadikshahidain@gmail.com>
2023-07-22 05:33:34 +0000

    add code and a test

--- packages/honeypot/src/servers/test_simple_http.py
+++ packages/honeypot/src/servers/test_simple_http.py
@@ -1,4 +1,5 @@
 from .simple_http import create_server_adapter, ServerAdapterInputs
+from event_clients.event_client_adapter_protocol import EventClientAdapterProtocol
 
 import unittest
 import http.client
@@ -6,9 +7,7 @@ import http.client
 
 class TestSimpleHttpServerAdapter(unittest.TestCase):
     def setUp(self) -> None:
-        self.__server_adapter = create_server_adapter(
-            ServerAdapterInputs(event_client=None)
-        )
+        self.__server_adapter = None
 
     def tearDown(self) -> None:
         # Here for cleanup regardless of what happens during a test
@@ -16,6 +15,10 @@ class TestSimpleHttpServerAdapter(unittest.TestCase):
 
     def test_GET_returns_200(self) -> None:
         # Arrange
+        self.__server_adapter = create_server_adapter(
+            ServerAdapterInputs(event_client=None)
+        )
+
         conn = http.client.HTTPConnection("127.0.0.1:8000", timeout=5)
 
         # Act
@@ -29,6 +32,36 @@ class TestSimpleHttpServerAdapter(unittest.TestCase):
         conn.close()  # unittest reports an unclosed socket even with this
 
         self.assertEqual(status_code, 200)
+
+    def test_GET_of_healthcheck_returns_200_and_doesnt_publish_an_event(self) -> None:
+        # Arrange
+        class MockEventClient(EventClientAdapterProtocol):
+            def __init__(self) -> None:
+                self.send_event_hit = False
+
+            def send_event(self, event_details: object) -> None:
+                self.send_event_hit = True
+
+        mock_event_client = MockEventClient()
+        self.__server_adapter = create_server_adapter(
+            ServerAdapterInputs(event_client=mock_event_client)
+        )
+
+        conn = http.client.HTTPConnection("127.0.0.1:8000", timeout=5)
+
+        # Act
+        self.__server_adapter.start()
+
+        conn.request("GET", "/healthcheck")
+
+        # Assert
+        response = conn.getresponse()
+        status_code = response.getcode()
+        conn.close()  # unittest reports an unclosed socket even with this
+
+        self.assertEqual(status_code, 200)
+
+        self.assertEqual(mock_event_client.send_event_hit, False)
 
 
 if __name__ == "__main__":
