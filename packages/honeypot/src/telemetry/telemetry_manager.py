df0c488a93a7695243aadc8aab139946516e63df
Grunet <sadikshahidain@gmail.com>
2023-07-22 18:47:04 +0000

    fix mypy

--- packages/honeypot/src/telemetry/telemetry_manager.py
+++ packages/honeypot/src/telemetry/telemetry_manager.py
@@ -14,7 +14,7 @@ class TelemetryManager(TelemetryManagerProtocol):
     def record_non_transaction_detail(self, structured_data: dict[str, object]) -> None:
         self.__record_detail(structured_data=structured_data)
 
-    def __record_detail(self, structured_data: dict[str, object]):
+    def __record_detail(self, structured_data: dict[str, object]) -> None:
         cloned_data = copy.deepcopy(structured_data)
 
         current_time = datetime.now(timezone.utc)
